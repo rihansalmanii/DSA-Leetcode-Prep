7352bd4af8f6d22c20825a1813a83e8ae9e7d995
Rihan <165789841+rihansalmanii@users.noreply.github.com>
2026-08-10 20:24:45 +0530

    Time: 6 ms (43.02%), Space: 44.4 MB (75.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        
+        if(s.length() != t.length()) return false;
 
-        boolean result = true;
+        int[] freq = new int[26];
 
-        if (s.length() != t.length())
-            return false;
-
-        for (int i = 0; i < s.length(); i++) {
-            map1.put(s.charAt(i), map1.getOrDefault(s.charAt(i), 0) + 1);
-
-            map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i), 0) + 1);
+        for(int i = 0; i<s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
 
-        
+        for(int count: freq) {
+            if(count != 0) {
+                return false;
+            }
+        }
 
-        return map1.equals(map2);
+        return true;
+        
     }
 }
