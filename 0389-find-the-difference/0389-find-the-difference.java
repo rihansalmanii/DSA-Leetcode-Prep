a67ac22a09c5a8d0d5b776ac3afae1c07b65ac37
Rihan <165789841+rihansalmanii@users.noreply.github.com>
2026-08-11 22:16:24 +0530

    Time: 16 ms (6.65%), Space: 43 MB (72.26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -5,16 +5,12 @@ class Solution {
         HashMap<Character, Integer> m1 = new HashMap<>();
         HashMap<Character, Integer> m2 = new HashMap<>();
 
-        for(char c: s.toCharArray()) {
-            m1.put(c, m1.getOrDefault(c, 0) + 1);
+        for(int i = 0; i<t.length(); i++) {
+            if(i < s.length()) {
+            m1.put(s.charAt(i), m1.getOrDefault(s.charAt(i), 0) + 1);
+            }
+            m2.put(t.charAt(i), m2.getOrDefault(t.charAt(i), 0) + 1);
         }
-
-        for(char c: t.toCharArray()) {
-            m2.put(c, m2.getOrDefault(c, 0) + 1);
-        }
-
-        System.out.println(m1);
-        System.out.println(m2);
 
         for(int i = 0; i < t.length(); i++) {
             if(m1.get(t.charAt(i)) != m2.get(t.charAt(i))) {
