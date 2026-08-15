fc255ab198cfb0c5e219ac47bdf6afdca4ae7f91
Rihan <165789841+rihansalmanii@users.noreply.github.com>
2026-08-15 22:29:38 +0530

    Time: 17 ms (12.91%), Space: 47.5 MB (7.62%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> map1 = new HashMap();
-        HashMap<Character, Integer> map2 = new HashMap();
+        HashMap<Character, Integer> map = new HashMap();
 
-        for (int i = 0; i < ransomNote.length(); i++) {
-            map1.put(ransomNote.charAt(i), map1.getOrDefault(ransomNote.charAt(i), 0) + 1);
-        }
 
         for (int i = 0; i < magazine.length(); i++) {
-            map2.put(magazine.charAt(i), map2.getOrDefault(magazine.charAt(i), 0) + 1);
+            map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
         }
 
         for (int i = 0; i < ransomNote.length(); i++) {
-            if (map2.containsKey(ransomNote.charAt(i))) {
-                if (map2.get(ransomNote.charAt(i)) < map1.get(ransomNote.charAt(i))) {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-
+           if(!map.containsKey(ransomNote.charAt(i))) {
+            return false;
+           } else {
+            map.put(ransomNote.charAt(i), map.get(ransomNote.charAt(i)) - 1);
+            if(map.get(ransomNote.charAt(i)) == 0) map.remove(ransomNote.charAt(i));
+           }
         }
 
         return true;
