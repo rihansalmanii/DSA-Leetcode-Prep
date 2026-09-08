03002035557e439c26e0adb3eab4d67637fc7ecc
Rihan <165789841+rihansalmanii@users.noreply.github.com>
2026-09-08 21:24:33 +0530

    Time: 0 ms (100%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,9 +1,5 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-        for(int i = 0; i<=n; i++) {
-            if(i >= 1000) count++;
-        }
-        return count;
+        return (n >= 1000) ? (n - 999) : 0;
     }
 }
